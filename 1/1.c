c4c315b1422c2236baab3ab9ff18b0f85a7236db
Aoife <aoifeebradley@gmail.com>
2020-12-03 15:08:09 -0500

    formatting and day 3!

--- 1/1.c
+++ 1/1.c
@@ -16,27 +16,30 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include <string.h>
 #include <inttypes.h>
 
-uint32_t hash( uint32_t a)
+uint32_t hash(uint32_t a)
 {
-    a = (a+0x7ed55d16) + (a<<12);
-    a = (a^0xc761c23c) ^ (a>>19);
-    a = (a+0x165667b1) + (a<<5);
-    a = (a+0xd3a2646c) ^ (a<<9);
-    a = (a+0xfd7046c5) + (a<<3);
-    a = (a^0xb55a4f09) ^ (a>>16);
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
     return a % 1048576;
 }
 
-typedef struct set {
+typedef struct set
+{
     uint32_t set[1048576];
 } hashset;
 
-int hashset_add(hashset *set, int item) {
-    if (item == NULL) {
+int hashset_add(hashset *set, int item)
+{
+    if (item == NULL)
+    {
         return -1;
     }
     uint32_t value = hash((uint32_t)item);
-    if (value >=1048576 )
+    if (value >= 1048576)
     {
         return -1;
     }
@@ -46,7 +49,8 @@ int hashset_add(hashset *set, int item) {
     return 1;
 }
 
-int hashset_find(hashset *set, int query) {
+int hashset_find(hashset *set, int query)
+{
     uint32_t query_hash = hash((uint32_t)query);
     if (set->set[query_hash] != query)
     {
@@ -55,8 +59,8 @@ int hashset_find(hashset *set, int query) {
     return query;
 }
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char *args[])
+{
 
     hashset set;
 
@@ -69,14 +73,16 @@ int main(int argc, char* args[]) {
     // open input file
     FILE *fp = fopen("input", "r");
 
-    if (!fp) {
+    if (!fp)
+    {
         printf("no file error!");
         exit(-1);
     }
     // input into hashset
-    while(fgets(line, sizeof(line), fp)) {
+    while (fgets(line, sizeof(line), fp))
+    {
         // remove carriage return and newline
-        line[ strcspn(line, "\r\n") ]=0;
+        line[strcspn(line, "\r\n")] = 0;
         // add to number array for later querying
         list[count] = atoi(line);
         // convert to integer from string and add to hashset
@@ -86,33 +92,32 @@ int main(int argc, char* args[]) {
     fclose(fp);
 
     // loop through and find the number equal to the hash of 2020 - current number in list
-    for (int i = 0; i < 199; i++) {
-       int result = hashset_find(&set, 2020-list[i]);
-       if (result != 0) 
-       { printf("%d\n", result);}
+    for (int i = 0; i < 199; i++)
+    {
+        int result = hashset_find(&set, 2020 - list[i]);
+        if (result != 0)
+        {
+            printf("%d\n", result);
+        }
     }
 
     printf("PART TWO \n\n");
-    
-    // subtract a number in list from 2020 then loop the list and 
+
+    // subtract a number in list from 2020 then loop the list and
     // return if a loop number subtracted from that first number exists
-    for (int j = 0; j < 199; j++) {
-        int num1 = 2020-list[j];
-        for (int k = 0; k < 199; k++) {
-            int result = hashset_find(&set, num1-list[k]);
-            if (result != 0) 
-            { 
-                printf("num1: %d\nnum2: %d\nnum3: %d\nmultiple: %d\n\n", list[j], list[k], result, list[j]*list[k]*result);
+    for (int j = 0; j < 199; j++)
+    {
+        int num1 = 2020 - list[j];
+        for (int k = 0; k < 199; k++)
+        {
+            int result = hashset_find(&set, num1 - list[k]);
+            if (result != 0)
+            {
+                printf("num1: %d\nnum2: %d\nnum3: %d\nmultiple: %d\n\n", list[j], list[k], result, list[j] * list[k] * result);
                 break;
             }
         }
     }
 
     return 0;
-
 }
-
-
-
-
-
--- 2/2.c
+++ 2/2.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* args[]) {
-    
+int main(int argc, char *args[])
+{
+
     // get input from file
-    FILE* file = fopen("input", "r");
+    FILE *file = fopen("input", "r");
     // line from file
     char line[128];
     // buffer for strtok
@@ -14,22 +15,24 @@ int main(int argc, char* args[]) {
     int count = 0;
     int count2 = 0;
 
-        if (!file) {
+    if (!file)
+    {
         printf("no file error!");
         exit(-1);
     }
 
-    while (fgets(line, 128, file)) {
-        line[ strcspn(line, "\r\n") ]=0;
+    while (fgets(line, 128, file))
+    {
+        line[strcspn(line, "\r\n")] = 0;
         strcpy(buff, line);
         //printf("%s\n", line);
         int min, max, charcount;
         char c;
-        
+
         charcount = 0;
 
         // get and set min
-        char* token = strtok(buff, "- :");
+        char *token = strtok(buff, "- :");
         min = atoi(token);
         //printf("%d\n", min);
 
@@ -37,27 +40,28 @@ int main(int argc, char* args[]) {
         token = strtok(NULL, "- :");
         max = atoi(token);
         //printf("%d\n", max);
-        
+
         // get and set char
         token = strtok(NULL, "- :");
         c = *token;
         //printf("%s\n", c);
 
-        for (int i = 0; i < strlen(line); i++) {
-            if(line[i] == c) {
+        for (int i = 0; i < strlen(line); i++)
+        {
+            if (line[i] == c)
+            {
                 charcount++;
-                
             }
         }
-        
+
         charcount--;
         //printf("%d\n", charcount);
 
-        if ( (charcount >= min) && (charcount <= max))
+        if ((charcount >= min) && (charcount <= max))
         {
             count++;
             //printf("current line: %s\n", line);
-        } 
+        }
 
         // part two code from here on out.
 
@@ -65,18 +69,20 @@ int main(int argc, char* args[]) {
         token = strtok(NULL, "- :");
 
         // if the character exists in only one position, increment the count
-        if (token[(min-1)] == c) {
-            if(token[(max-1)] != c)
+        if (token[(min - 1)] == c)
+        {
+            if (token[(max - 1)] != c)
             {
-            count2++;
-            //printf("yes\n");
+                count2++;
+                //printf("yes\n");
             }
         }
-        else if (token[(max-1)] == c) {
-            if(token[(min-1)] != c)
+        else if (token[(max - 1)] == c)
+        {
+            if (token[(min - 1)] != c)
             {
-            count2++;
-            //printf("yes\n");
+                count2++;
+                //printf("yes\n");
             }
         }
     }
@@ -85,6 +91,4 @@ int main(int argc, char* args[]) {
     printf("part 2 password count: %d\n", count2);
 
     fclose(file);
-
-
 }
